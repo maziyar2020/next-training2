e9ab6227bd8fd7e29d1ee7201ab89e2e36d130bf
maziyar <maziroadtodivine@gmail.com>
2023-02-10 14:00:43 +0330

    Change data fetching method and set btn event.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,29 @@
 import axios from "axios"
 import Navbar from "components/common/Navbar"
-import {CheckIcon , TrashIcon , PencilAltIcon} from '@heroicons/react/outline'
-import useSWR from 'swr'
+import { CheckIcon, TrashIcon, PencilAltIcon } from '@heroicons/react/outline'
+
+import LoadingSpinner from "components/common/Loading/Loading"
+import { useEffect, useState } from "react"
 
 export default function Home() {
+  const [loading, setLoading] = useState(true)
+  const [data, setData] = useState(null)
 
-  const fetcher = async () => {
-    const { data } = await axios.get('/api/todos')
-    const { todos } = data
-    return todos
-  }
+  useEffect(() => {
+    axios.get('/api/todos')
+      .then(res => {
+        setData(res.data)
+        setLoading(false)
+      })
+      .catch(err => console.log(err))
+  }, [])
 
-  const { data, error } = useSWR('getProfileData', fetcher)
+  if (loading) return <div className="loading"> <LoadingSpinner /> </div>
 
-  if (error) return <div>{error}</div>
-  if (!data) return <div>wait</div>
+  const deleteTodos = (item) => console.log(item);
+  const doneTodos = (item) => console.log(item);
+  const editTodos = (item) => console.log(item);
+
 
   return (
     <main className="bg-gray-50 min-h-screen">
@@ -25,18 +34,21 @@ export default function Home() {
         <section className="flex justify-center items-center">
           <div className="w-full max-w-screen-md bg-white p-2 md:p-4 rounded-xl">
             {/* loop over Array of data */}
-            {data.map(item =>
+            {data.todos.map(item =>
               <div key={item.id} className="flex items-center justify-between border border-gray-100 mb-4 p-3 md:p-4 rounded-md" >
                 {/* Todo name */}
                 <span>{item.title} </span>
                 <div className="flex gap-x-3 items-center">
-                  <button className="">
+                  {/* Task is Done */}
+                  <button className="" onClick={() => { doneTodos(item.id) }}>
                     <CheckIcon className="w-6 h-6 stroke-green-400" />
                   </button>
-                  <button className="">
+                  {/* Delete Task */}
+                  <button className="" onClick={() => { deleteTodos(item.id) }}>
                     <TrashIcon className="w-6 h-6 stroke-red-400" />
                   </button>
-                  <button className="">
+                  {/* Edit Task */}
+                  <button className="" onClick={() => { editTodos(item.id) }}>
                     <PencilAltIcon className="w-6 h-6 stroke-blue-400" />
                   </button>
                 </div>
