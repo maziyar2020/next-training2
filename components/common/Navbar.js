ce4d758b993dc0994630ef1ebb0940b3fc2bbc8e
maziyar <maziroadtodivine@gmail.com>
2023-02-10 13:26:29 +0330

    Create data and navbar for Todo app.

---
+++ components/common/Navbar.js
@@ -0,0 +1,9 @@
+const Navbar = () => {
+    return (
+        <nav className="w-full bg-white shadow-sm flex justify-center py-4 mb-6">
+            <h1 className="font-bold">Todo List using next.js & TailwindCss </h1>
+        </nav>
+    );
+}
+
+export default Navbar;
