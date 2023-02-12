6da0f0ef24b50b7b1a1b4d93b4f9bab77402e7e6
maziyar <maziroadtodivine@gmail.com>
2023-02-13 01:28:23 +0330

    Create a logic method for delete todos.

--- src/pages/api/todos/[todoId].js
+++ src/pages/api/todos/[todoId].js
@@ -1,10 +1,15 @@
-import { todos } from "data/todos";
 
-export default function handler(req, res) {
-    const { todoId } = req.query
-    if (req.method === "DELETE") {
-        const index = todos.findIndex(todo => todo.id === parseInt(todoId))
-        todos.splice(index, 1)
+import dbConnect from "server/utils/dbConnect"
+
+import Todo from '../../../../server/models/todo'
+dbConnect()
+
+export default async function handler(req, res) {
+    const { method, query } = req
+
+    if (method === "DELETE") {
+        await Todo.findByIdAndDelete(query.todoId)
+        const todos = await Todo.find({})
+        return res.status(200).json({message : 'Todo deleted' , todos})
     }
-    return res.status(200).json({ messege: "task compeleted", todos })
 }
